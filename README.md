b537601ad37e7790b97122c051fa7502051ba07c
Nandor Licker <n@ndor.email>
2018-10-18 13:08:53 +0100

    [genm-opt] Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+GenM Optimiser
+===
+
+
---
+++ opt/main.cpp
@@ -0,0 +1,10 @@
+// This file if part of the genm-opt project.
+// Licensing information can be found in the LICENSE file.
+// (C) 2018 Nandor Licker. All rights reserved.
+
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+  return EXIT_SUCCESS;
+}
