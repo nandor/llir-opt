37c322c568a63c5c28a62b9249d9e34642ae68e2
Nandor Licker <n@ndor.email>
2019-02-27 12:55:25 +0000

    [genm-opt] Implemented sparse bitsets.

--- passes/global_data_elim/bitset.h
+++ passes/global_data_elim/bitset.h
@@ -6,8 +6,9 @@
 
 #include <cassert>
 #include <cstdint>
-#include <set>
+#include <map>
 
+#include <set>
 #include <llvm/Support/raw_ostream.h>
 /**
  * Sparse bit set implementation.
@@ -17,11 +18,27 @@ class BitSet final {
 private:
   /// Node in the sparse bitset.
   struct Node {
+    Node() : Fst(0ull), Snd(0ull) {}
+
+    Node(Node &&node) = delete;
+
+    Node(const Node &node) : Fst(node.Fst), Snd(node.Snd) {}
+
+    void operator = (const Node &node) = delete;
+
     /// First 64 elements.
     uint64_t Fst;
     /// Last 64 elements.
     uint64_t Snd;
+
+    bool operator == (const Node &that) const
+    {
+      return Fst == that.Fst && Snd == that.Snd;
+    }
   };
+
+  /// Iterator over the structure holding the node.
+  using NodeIt = typename std::map<uint32_t, Node>::const_iterator;
 
 public:
   /// Integer type identifying items.
@@ -31,38 +48,70 @@ public:
   class iterator final {
   public:
     iterator(const iterator &that)
-      : it_(that.it_)
+      : set_(that.set_)
+      , it_(that.it_)
+      , current_(that.current_)
     {
     }
 
-    iterator(std::set<uint32_t>::iterator it)
-      : it_(it)
+    iterator(const BitSet<T> &set, uint64_t current)
+      : set_(set)
+      , it_(set.nodes_.find(current >> 7))
+      , current_(current)
     {
     }
 
     Item operator * () const
     {
-      return *it_;
+      return current_;
     }
 
     iterator operator ++ ()
     {
-      it_++;
+      for (;;) {
+        ++current_;
+        if (current_ > set_.last_) {
+          return *this;
+        }
+
+        uint64_t pos = current_ & ((1 << 7) - 1);
+        if (pos == 0) {
+          ++it_;
+          current_ = it_->first << 7;
+        }
+
+        if (pos < 64) {
+          if ((it_->second.Fst & (1ull << pos)) != 0) {
+            return *this;
+          }
+        } else {
+          if ((it_->second.Snd & (1ull << (pos - 64ull))) != 0) {
+            return *this;
+          }
+        }
+      }
+
       return *this;
     }
 
     bool operator != (const iterator &that) const
     {
-      return it_ != that.it_;
+      return current_ != that.current_;
     }
 
   private:
+    /// Reference to the set.
+    const BitSet<T> &set_;
     /// Iterator over the hash map.
-    std::set<uint32_t>::iterator it_;
+    NodeIt it_;
+    /// Current item.
+    uint64_t current_;
   };
 
   /// Constructs a new bitset.
   explicit BitSet()
+    : first_(std::numeric_limits<Item>::max())
+    , last_(std::numeric_limits<Item>::min())
   {
   }
 
@@ -72,45 +121,86 @@ public:
   }
 
   /// Start iterator.
-  iterator begin()
+  iterator begin() const
   {
-    return iterator(items_.begin());
+    if (first_ > last_) {
+      return end();
+    } else {
+      return iterator(*this, first_);
+    }
   }
 
   /// End iterator.
-  iterator end()
+  iterator end() const
   {
-    return iterator(items_.end());
+    return iterator(*this, static_cast<uint64_t>(last_) + 1ull);
   }
 
   /// Inserts an item into the bitset.
   void Insert(Item item)
   {
-    items_.insert(item);
+    first_ = std::min(first_, item);
+    last_ = std::max(last_, item);
+
+    auto &node = nodes_[item >> 7];
+    uint64_t idx = item & ((1 << 7) - 1);
+    if (idx < 64) {
+      node.Fst |= (1ull << (idx - 0));
+    } else {
+      node.Snd |= (1ull << (idx - 64));
+    }
   }
 
   /// Efficiently computes the union of two bitsets.
   bool Union(const BitSet &that)
   {
     bool changed = false;
-    for (auto item : that.items_) {
-      changed |= items_.insert(item).second;
+
+    for (auto &thatNode : that.nodes_) {
+      auto &thisNode = nodes_[thatNode.first];
+      uint64_t oldFst = thisNode.Fst;
+      uint64_t oldSnd = thisNode.Snd;
+
+      thisNode.Fst |= thatNode.second.Fst;
+      thisNode.Snd |= thatNode.second.Snd;
+
+      changed |= oldFst != thisNode.Fst;
+      changed |= oldSnd != thisNode.Snd;
     }
+
+    first_ = std::min(first_, that.first_);
+    last_ = std::max(last_, that.last_);
+
     return changed;
   }
 
   /// Checks if two bitsets are equal.
   bool operator == (const BitSet &that) const
   {
-    return items_ == that.items_;
+    if (first_ != that.first_) {
+      return false;
+    }
+    if (last_ != that.last_) {
+      return false;
+    }
+
+    return std::equal(
+        nodes_.begin(), nodes_.end(),
+        that.nodes_.begin(), that.nodes_.end()
+    );
   }
 
-  /// Disallow copy construct.
+  /// Disallow copy and assign.
+  BitSet(BitSet &&) = delete;
   BitSet(const BitSet &) = delete;
-  /// Disallow assign.
   void operator = (const BitSet &) = delete;
+  void operator = (BitSet &&) = delete;
 
 private:
+  /// First element.
+  Item first_;
+  /// Last element.
+  Item last_;
   /// Nodes stored in the bit set.
-  std::set<uint32_t> items_;
+  std::map<uint32_t, Node> nodes_;
 };
