ad24ea2cbb36a635b0f0cf7e147171dfbdffc828
Nandor Licker <n@ndor.email>
2019-12-11 16:32:58 +0000

    [genm-opt] Added test for doubles.

---
+++ test/ml/double.ml
@@ -0,0 +1,9 @@
+(* This file is part of the genm-opt project. *)
+(* Licensing information can be found in the LICENSE file. *)
+(* (C) 2018 Nandor Licker. ALl rights reserved. *)
+
+external double_ident : 'a -> 'a = "double_ident"
+
+
+let () =
+  assert(double_ident 1. +. double_ident 1. = double_ident 2.)
