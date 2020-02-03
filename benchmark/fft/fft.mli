443b51802ba47350935eb75766aad0bab62d789f
Nandor Licker <n@ndor.email>
2020-02-03 15:00:08 +0000

    [genm-opt] Removed unused benchmarks.

--- benchmark/fft/fft.mli
+++
@@ -1,7 +0,0 @@
-(* This file is part of the genm project. *)
-(* Licensing information can be found in the LICENSE file. *)
-(* (C) 2018 Nandor Licker. ALl rights reserved. *)
-
-
-
-val fft : Complex.vec -> Complex.vec
