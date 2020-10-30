2fb0be85c64431eddda81e7208135ab7eebd234c
Nandor Licker <n@ndor.email>
2020-10-30 19:51:48 +0000

    [llir-opt] Added explicit run lines to test harness

--- test.py
+++ test.py
@@ -8,98 +8,162 @@ import subprocess
 import sys
 import tempfile
 
-IS_DEBUG = 'Debug' in os.getcwd()
 PROJECT = os.path.dirname(os.path.abspath(__file__))
-OPT_EXE = os.path.join(
-    PROJECT,
-    'Debug' if IS_DEBUG else 'Release',
-    'tools',
-    'llir-opt',
-    'llir-opt'
-)
-OPT_LEVEL = '-O3'
 
 
-def run_proc(*args, **kwargs):
-  """Runs a process, dumping output if it fails."""
-  sys.stdout.flush()
+def which(exe):
+  """Finds an executable in the path."""
 
-  proc = subprocess.Popen(
-      stdout=subprocess.PIPE,
-      stderr=subprocess.PIPE,
-      *args,
-      **kwargs
-  )
-  stdout, stderr = proc.communicate()
-  if proc.returncode != 0:
-    print("\n%s: exited with %d" % (' '.join(args[0]), proc.returncode))
-    print("\nstdout:\n%s" % stdout.decode('utf-8'))
-    print("\nstderr:\n%s" % stderr.decode('utf-8'))
+  if exe.startswith('/'):
+    return exe
+
+  for path in os.environ["PATH"].split(os.pathsep):
+    path = os.path.join(path, exe)
+    if os.path.isfile(path) and os.access(path, os.X_OK):
+      return path
+
+  print('{} not found'.format(exe))
+  sys.exit(-1)
+
+
+def get_opt_exe():
+  """Returns the path to the opt binary."""
+
+  if 'Debug' in os.getcwd():
+    return os.path.join(PROJECT, 'Debug', 'tools', 'llir-opt', 'llir-opt')
+  if 'Release' in os.getcwd():
+    return os.path.join(PROJECT, 'Release', 'tools', 'llir-opt', 'llir-opt')
+  return os.path.join(PROJECT, 'tools', 'llir-opt', 'llir-opt')
+
+OPT_EXE = get_opt_exe()
+CLANG_EXE = which('clang')
+
+
+
+class RunError(Exception):
+  pass
 
 
 def _process(line):
   return ' '.join(w for w in line.replace('\t', ' ').split(' ') if w)
 
 
-def run_asm_test(path, output_dir):
+def run_opt_test(path, output_dir, comment):
   """Runs an assembly test."""
 
-  args = []
+  # Open the file and parse the special lines, extracting the commands
+  # to run and the strings to identify in sequence in the file.
+  run_line = None
   checks = []
   with open(path, 'r') as f:
-    for line in f.readlines():
-      line = line.strip()
-      if line.startswith('# ARGS:'):
-        for arg in ' '.join(line.split('# ARGS:')[1:]).split(' '):
-          arg = arg.strip()
-          if arg:
-            args.append(arg)
-      if line.startswith('# CHECK:'):
-        checks.append(' '.join(line.split('# CHECK:')[1:]).strip())
+    for source_line in f.readlines():
+      line = ' '.join(source_line.strip().split())
+      if not line.startswith(comment):
+        continue
+      line = line[len(comment):].strip()
+      if not ':' in line:
+        continue
+      cmd = line.split(':')[0].strip()
+      args = ':'.join(line.split(':')[1:]).strip()
+      if cmd == 'RUN':
+        run_line = args
+        continue
+      if cmd == 'CHECK':
+        checks.append(args)
+        continue
+      if cmd == 'DISABLED':
+        return True
+      raise RunError(f'Invalid check line: {source_line}')
 
-  llir_lnk = os.path.join(output_dir, 'out.S')
-  run_proc([OPT_EXE, path, '-o', llir_lnk] + args)
+  if run_line is None:
+    raise RunError(f'Missing run command: {path}')
+
+  run_line = run_line.replace('%opt', OPT_EXE)
+  run_line = run_line.replace('%clang', CLANG_EXE)
+
+  # Set up a pipeline to execute all commands.
+  commands = run_line.split('|')
+
+  with open(path, 'r') as f:
+    procs = []
+
+    stdin = f
+    for command in commands:
+      command = command.strip()
+      args = command.split(' ')
+      exe = args[0]
+      args = [exe] + args[1:]
+      try:
+        proc = subprocess.Popen(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=stdin
+        )
+        stdin = proc.stdout
+        procs.append((command, proc))
+      except OSError as e:
+        raise RunError('Cannot launch {}: {}'.format(exe, e))
+
+    all_stderr = b''
+    stdout, stderr = procs[-1][1].communicate()
+    for command, proc in procs[:-1]:
+      all_stderr += proc.stderr.read()
+      code = proc.wait()
+      if code != 0:
+        raise RunError('Command {} failed: {}'.format(command, code))
+    all_stderr += stderr
+
+  if all_stderr:
+    print('FAIL: {}'.format(all_stderr.decode('utf-8')))
+    return False
 
   if checks:
-    try:
-      with open(llir_lnk, 'r') as f:
-        lines = f.readlines()
-      checked = 0
-      for check in checks:
-        while checked < len(lines) and check not in _process(lines[checked]):
-          checked += 1
+    lines = stdout.decode('utf-8').split('\n')
+    checked = 0
+    for check in checks:
+      while checked < len(lines) and check not in _process(lines[checked]):
+        checked += 1
 
-        if checked >= len(lines):
-          print('FAIL: {} not found ({})'.format(check, ' '.join(args)))
-    except:
-      print('FAIL: no output ({})'.format(args))
+      if checked >= len(lines):
+        print('FAIL: {} not found ({})'.format(check, ' '.join(args)))
+        return False
+
+  return True
 
 
 def run_test(path, output_dir=None):
   """Runs a test, detecting type from the extension."""
 
-  print(path)
-
   def run_in_directory(output_dir):
     """Runs a test in a temporary directory."""
     if path.endswith('.S'):
-      return run_asm_test(path, output_dir)
-    raise Exception('Unknown extension: %s' % path)
+      return run_opt_test(path, output_dir, '#')
+    if path.endswith('.c'):
+      return run_opt_test(path, output_dir, '//')
+    raise Exception(f'Unknown extension: {path}')
 
-  if output_dir:
-    if not os.path.exists(output_dir):
-      os.makedirs(output_dir)
-    run_in_directory(output_dir)
-  else:
-    with tempfile.TemporaryDirectory() as root:
-      run_in_directory(root)
+  print(path)
+
+  try:
+    if output_dir:
+      if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+      return run_in_directory(output_dir)
+    else:
+      with tempfile.TemporaryDirectory() as root:
+        return run_in_directory(root)
+  except RunError as e:
+    print(e)
+    return False
 
 
 def run_tests(tests):
   """Runs a test suite."""
   for test in tests:
-    run_test(os.path.abspath(test))
-
+    if not run_test(os.path.abspath(test)):
+      return False
+  return True
 
 
 if __name__ == '__main__':
@@ -115,4 +179,5 @@ if __name__ == '__main__':
           if not file.endswith('_ext.c'):
             yield os.path.join(directory, file)
 
-    run_tests(find_tests())
+    if not run_tests(find_tests()):
+      sys.exit(-1)
