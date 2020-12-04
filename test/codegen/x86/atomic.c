c52e03c20d270a46c76e73e63e900413f4b8dd24
Nandor Licker <n@ndor.email>
2020-12-04 12:29:22 +0000

    [llir-opt][x86] cmpxchg tests

--- test/codegen/x86/atomic.c
+++ test/codegen/x86/atomic.c
@@ -24,3 +24,28 @@ void store32(int *ptr, int val)
 {
   __atomic_store_n(ptr, val, __ATOMIC_RELEASE);
 }
+
+long long or8(char *ptr, char val)
+{
+  return __atomic_fetch_or(ptr, val, __ATOMIC_SEQ_CST);
+}
+
+long long or32(int *ptr, int val)
+{
+  return __atomic_fetch_or(ptr, val, __ATOMIC_SEQ_CST);
+}
+
+long long or64(long long *ptr, long long val)
+{
+  return __atomic_fetch_or(ptr, val, __ATOMIC_SEQ_CST);
+}
+
+long long and32(int *ptr, int val)
+{
+  return __atomic_fetch_and(ptr, val, __ATOMIC_SEQ_CST);
+}
+
+long long and64(long long *ptr, long long val)
+{
+  return __atomic_fetch_and(ptr, val, __ATOMIC_SEQ_CST);
+}
