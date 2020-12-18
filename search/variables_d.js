3a05905800fec40206b731b7bd1e2bb7ead817f7
nandor <nl364@cam.ac.uk>
2020-12-18 13:37:29 +0000

    Deploying to gh-pages from  @ 0074996197d8152e5e7eb505b092957c0a4aa244 🚀

--- search/variables_d.js
+++ search/variables_d.js
@@ -6,6 +6,7 @@ var searchData=
   ['off',['Off',['../classLattice.html#a1dfc88558a18afde2dfac48dc32f2e79',1,'Lattice']]],
   ['offset',['Offset',['../structCallLowering_1_1ArgPart.html#a4baabd1e1904baa5d32b07e0a4c55b11',1,'CallLowering::ArgPart']]],
   ['ol_5f',['ol_',['../classISel.html#a7722ed77a45943cb9055ae174995fe3a',1,'ISel']]],
+  ['opt',['Opt',['../structPassConfig.html#a2691f3ccd4e522f3758f413df043b193',1,'PassConfig']]],
   ['order_5f',['order_',['../classInst.html#a53768cab3297a47e5386c2b72a51ebcc',1,'Inst']]],
   ['origins',['Origins',['../structFlowGraph_1_1Node.html#a691820cddd06882db8bc47af3d0d550d',1,'FlowGraph::Node']]],
   ['os_5f',['os_',['../classEmitter.html#ab06f59aef4f752d54ccd80daf86a660f',1,'Emitter::os_()'],['../classRuntimePrinter.html#a8026e2d23d1a2da85974a920e079b623',1,'RuntimePrinter::os_()']]]
