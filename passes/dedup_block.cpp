5c0236ad077373e0a6d22df6c566dff9b758e564
Nandor Licker <n@ndor.email>
2021-01-14 18:46:54 +0000

    [llir-opt] Improved block deduplication performance

--- passes/dedup_block.cpp
+++ passes/dedup_block.cpp
@@ -12,6 +12,7 @@
 #include "core/insts.h"
 #include "core/inst_compare.h"
 #include "core/prog.h"
+#include "core/adt/hash.h"
 #include "passes/dedup_block.h"
 
 
@@ -36,10 +37,57 @@ bool DedupBlockPass::Run(Prog &prog)
 }
 
 // -----------------------------------------------------------------------------
+static size_t Hash(Block &block)
+{
+  size_t hash = block.size();
+  for (Inst &inst : block) {
+    ::hash_combine(hash, static_cast<uint8_t>(inst.GetKind()));
+    for (Ref<Value> op : inst.operand_values()) {
+      switch (op->GetKind()) {
+        case Value::Kind::INST: {
+          continue;
+        }
+        case Value::Kind::GLOBAL: {
+          ::hash_combine(hash, std::hash<Ref<Value>>{}(op));
+          continue;
+        }
+        case Value::Kind::EXPR: {
+          switch (::cast<Expr>(op)->GetKind()) {
+            case Expr::Kind::SYMBOL_OFFSET: {
+              auto soe = ::cast<SymbolOffsetExpr>(op);
+              ::hash_combine(hash, std::hash<Global *>{}(soe->GetSymbol()));
+              ::hash_combine(hash, soe->GetOffset());
+              continue;
+            }
+          }
+          llvm_unreachable("invalid expression kind");
+        }
+        case Value::Kind::CONST: {
+          switch (::cast<Constant>(op)->GetKind()) {
+            case Constant::Kind::INT: {
+              auto ci = ::cast<ConstantInt>(op);
+              ::hash_combine(hash, std::hash<int64_t>{}(ci->GetInt()));
+              continue;
+            }
+            case Constant::Kind::FLOAT: {
+              auto cf = ::cast<ConstantFloat>(op);
+              ::hash_combine(hash, std::hash<double>{}(cf->GetDouble()));
+              continue;
+            }
+          }
+          llvm_unreachable("invalid constant kind");
+        }
+      }
+    }
+  }
+  return hash;
+}
+
+// -----------------------------------------------------------------------------
 bool DedupBlockPass::Run(Func &func)
 {
   bool changed = false;
-  std::vector<Block *> candidates;
+  std::unordered_map<size_t, std::vector<Block *>> candidates;
   for (auto it = llvm::scc_begin(&func); !it.isAtEnd(); ++it) {
     if (it->size() != 1) {
       continue;
@@ -47,7 +95,8 @@ bool DedupBlockPass::Run(Func &func)
 
     bool replaced = false;
     Block *b1 = (*it)[0];
-    for (Block *b2 : candidates) {
+    auto &bucket = candidates[Hash(*b1)];
+    for (Block *b2 : bucket) {
       if (IsEqual(b1, b2)) {
         for (Block *b1succ : b1->successors()) {
           for (PhiInst &phi : b1succ->phis()) {
@@ -69,7 +118,7 @@ bool DedupBlockPass::Run(Func &func)
       }
     }
     if (!replaced) {
-      candidates.push_back(b1);
+      bucket.push_back(b1);
     }
     changed = changed || replaced;
   }
