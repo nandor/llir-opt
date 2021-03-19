036c30a8a7b708be035190edab20726053ca0534
Nandor Licker <n@ndor.email>
2021-03-19 11:09:31 +0200

    [llir-opt][global-forward] Moved reverse stage to separate file

--- passes/global_forward/forwarder.cpp
+++ passes/global_forward/forwarder.cpp
@@ -7,8 +7,6 @@
 #include <limits>
 #include <unordered_set>
 
-
-#include <llvm/ADT/Statistic.h>
 #include <llvm/ADT/SCCIterator.h>
 #include <llvm/Support/Debug.h>
 
@@ -20,9 +18,6 @@
 #include "passes/global_forward/forwarder.h"
 
 #define DEBUG_TYPE "global-forward"
-
-STATISTIC(NumStoresFolded, "Stores folded");
-STATISTIC(NumStoresKilled, "Dead stores removed");
 
 
 // -----------------------------------------------------------------------------
@@ -403,118 +398,6 @@ bool GlobalForwarder::Forward()
       }
     } else {
       state.Active--;
-    }
-  }
-  return changed;
-}
-
-// -----------------------------------------------------------------------------
-bool GlobalForwarder::Reverse()
-{
-  std::vector<ReverseNodeState *> nodes;
-  std::unordered_set<ReverseNodeState *> visited;
-  std::function<void(ReverseNodeState *node)> dfs =
-    [&, this] (ReverseNodeState *node)
-    {
-      if (!visited.insert(node).second) {
-        return;
-      }
-      // Compute information for all the successors in the DAG.
-      for (auto *succ : node->Succs) {
-        dfs(succ);
-      }
-      nodes.push_back(node);
-    };
-
-  auto *entry = &GetReverseNode(entry_, GetDAG(entry_).rbegin()->Index);
-  dfs(entry);
-
-  LLVM_DEBUG(llvm::dbgs() << "===================\n");
-  LLVM_DEBUG(llvm::dbgs() << "Reverse:\n");
-  LLVM_DEBUG(llvm::dbgs() << "===================\n");
-
-  bool changed = false;
-  for (ReverseNodeState *node : nodes) {
-    LLVM_DEBUG(llvm::dbgs() << "===================\n");
-    LLVM_DEBUG(llvm::dbgs() << node->Node << "\n");
-    LLVM_DEBUG(llvm::dbgs() << "===================\n");
-    // Merge information from successors.
-    std::optional<ReverseNodeState> merged;
-    LLVM_DEBUG(llvm::dbgs() << "Merged:\n");
-    for (auto *succ : node->Succs) {
-      LLVM_DEBUG(llvm::dbgs() << "\t" << succ->Node << "\n");
-      if (merged) {
-        merged->Merge(*succ);
-      } else {
-        merged.emplace(*succ);
-      }
-    }
-    #ifndef DEBUG
-    if (merged) {
-      LLVM_DEBUG(merged->dump(llvm::dbgs()));
-      LLVM_DEBUG(node->dump(llvm::dbgs()));
-    }
-    #endif
-    // Apply the transfer function.
-    if (merged) {
-      auto stores = node->Stores;
-      for (auto &&[id, stores] : merged->Stores) {
-        if (node->Stored.Contains(id)) {
-          continue;
-        }
-        auto storeIt = node->Stores.find(id);
-        for (auto &[start, instAndEnd] : stores) {
-          auto &[inst, end] = instAndEnd;
-
-          bool killed = false;
-          if (storeIt != node->Stores.end()) {
-            for (auto &[nodeStart, nodeInstAndEnd] : storeIt->second) {
-              auto &[nodeInst, nodeEnd] = nodeInstAndEnd;
-              if (end <= nodeStart || nodeEnd <= start) {
-                continue;
-              }
-              if (start == nodeStart && end == nodeEnd) {
-                killed = true;
-                break;
-              }
-              llvm_unreachable("not implemented");
-            }
-          }
-          if (!killed) {
-            node->Stores[id].emplace(start, instAndEnd);
-          }
-        }
-      }
-
-      node->Loaded.Union(merged->Loaded);
-    }
-    LLVM_DEBUG(llvm::dbgs() << "Final:\n");
-    #ifndef DEBUG
-    LLVM_DEBUG(node->dump(llvm::dbgs()));
-    #endif
-  };
-
-  for (auto &[id, stores] : entry->Stores) {
-    auto *object = idToObject_[id];
-    if (!object) {
-      continue;
-    }
-
-    for (auto &[off, instAndEnd] : stores) {
-      auto &[store, end] = instAndEnd;
-      if (!store) {
-        continue;
-      }
-      auto mov = ::cast_or_null<MovInst>(store->GetValue());
-      if (!mov || !mov->GetArg()->IsConstant()) {
-        continue;
-      }
-      if (object->Store(off, mov->GetArg(), mov.GetType())) {
-        LLVM_DEBUG(llvm::dbgs() << "Folded store: " << *store->GetAddr() << "\n");
-        store->eraseFromParent();
-        NumStoresFolded++;
-        changed = true;
-      }
     }
   }
   return changed;
---
+++ passes/global_forward/reverse.cpp
@@ -0,0 +1,126 @@
+// This file if part of the llir-opt project.
+// Licensing information can be found in the LICENSE file.
+// (C) 2018 Nandor Licker. All rights reserved.
+
+#include <llvm/ADT/Statistic.h>
+#include <llvm/Support/Debug.h>
+
+#include "passes/global_forward/forwarder.h"
+
+#define DEBUG_TYPE "global-forward"
+
+STATISTIC(NumStoresFolded, "Stores folded");
+
+
+
+// -----------------------------------------------------------------------------
+bool GlobalForwarder::Reverse()
+{
+  std::vector<ReverseNodeState *> nodes;
+  std::unordered_set<ReverseNodeState *> visited;
+  std::function<void(ReverseNodeState *node)> dfs =
+    [&, this] (ReverseNodeState *node)
+    {
+      if (!visited.insert(node).second) {
+        return;
+      }
+      // Compute information for all the successors in the DAG.
+      for (auto *succ : node->Succs) {
+        dfs(succ);
+      }
+      nodes.push_back(node);
+    };
+
+  auto *entry = &GetReverseNode(entry_, GetDAG(entry_).rbegin()->Index);
+  dfs(entry);
+
+  LLVM_DEBUG(llvm::dbgs() << "===================\n");
+  LLVM_DEBUG(llvm::dbgs() << "Reverse:\n");
+  LLVM_DEBUG(llvm::dbgs() << "===================\n");
+
+  bool changed = false;
+  for (ReverseNodeState *node : nodes) {
+    LLVM_DEBUG(llvm::dbgs() << "===================\n");
+    LLVM_DEBUG(llvm::dbgs() << node->Node << "\n");
+    LLVM_DEBUG(llvm::dbgs() << "===================\n");
+    // Merge information from successors.
+    std::optional<ReverseNodeState> merged;
+    LLVM_DEBUG(llvm::dbgs() << "Merged:\n");
+    for (auto *succ : node->Succs) {
+      LLVM_DEBUG(llvm::dbgs() << "\t" << succ->Node << "\n");
+      if (merged) {
+        merged->Merge(*succ);
+      } else {
+        merged.emplace(*succ);
+      }
+    }
+    #ifndef DEBUG
+    if (merged) {
+      LLVM_DEBUG(merged->dump(llvm::dbgs()));
+      LLVM_DEBUG(node->dump(llvm::dbgs()));
+    }
+    #endif
+    // Apply the transfer function.
+    if (merged) {
+      auto stores = node->Stores;
+      for (auto &&[id, stores] : merged->Stores) {
+        if (node->Stored.Contains(id)) {
+          continue;
+        }
+        auto storeIt = node->Stores.find(id);
+        for (auto &[start, instAndEnd] : stores) {
+          auto &[inst, end] = instAndEnd;
+
+          bool killed = false;
+          if (storeIt != node->Stores.end()) {
+            for (auto &[nodeStart, nodeInstAndEnd] : storeIt->second) {
+              auto &[nodeInst, nodeEnd] = nodeInstAndEnd;
+              if (end <= nodeStart || nodeEnd <= start) {
+                continue;
+              }
+              if (start == nodeStart && end == nodeEnd) {
+                killed = true;
+                break;
+              }
+              llvm_unreachable("not implemented");
+            }
+          }
+          if (!killed) {
+            node->Stores[id].emplace(start, instAndEnd);
+          }
+        }
+      }
+
+      node->Loaded.Union(merged->Loaded);
+    }
+    LLVM_DEBUG(llvm::dbgs() << "Final:\n");
+    #ifndef DEBUG
+    LLVM_DEBUG(node->dump(llvm::dbgs()));
+    #endif
+  };
+
+  for (auto &[id, stores] : entry->Stores) {
+    auto *object = idToObject_[id];
+    if (!object) {
+      continue;
+    }
+
+    for (auto &[off, instAndEnd] : stores) {
+      auto &[store, end] = instAndEnd;
+      if (!store) {
+        continue;
+      }
+      auto mov = ::cast_or_null<MovInst>(store->GetValue());
+      if (!mov || !mov->GetArg()->IsConstant()) {
+        continue;
+      }
+      if (object->Store(off, mov->GetArg(), mov.GetType())) {
+        LLVM_DEBUG(llvm::dbgs() << "Folded store: " << *store->GetAddr() << "\n");
+        store->eraseFromParent();
+        NumStoresFolded++;
+        changed = true;
+      }
+    }
+  }
+  return changed;
+}
