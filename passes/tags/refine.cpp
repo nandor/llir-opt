daf000285dc7f7a2ca0d28efd6faaf122ea3de91
Nandor Licker <n@ndor.email>
2021-05-15 10:08:25 +0300

    [llir-opt][eliminate-tags] Dummy refine visitor

---
+++ passes/tags/refine.cpp
@@ -0,0 +1,13 @@
+// This file if part of the llir-opt project.
+// Licensing information can be found in the LICENSE file.
+// (C) 2018 Nandor Licker. All rights reserved.
+
+#include "core/atom.h"
+#include "core/object.h"
+#include "core/data.h"
+#include "passes/tags/refine.h"
+#include "passes/tags/tagged_type.h"
+#include "passes/tags/analysis.h"
+
+using namespace tags;
+
