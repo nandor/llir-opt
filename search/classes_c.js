a7c8a730994696f757e4ff9b1b06f36df20a2b27
nandor <nl364@cam.ac.uk>
2021-06-04 10:05:43 +0000

    Deploying to gh-pages from  @ c4785e532dbde22c6574c8dd2eecd2fadc17e8d7 🚀

--- search/classes_c.js
+++ search/classes_c.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['mask_1252',['Mask',['../structBinaryVisitor_1_1Mask.html',1,'BinaryVisitor']]],
-  ['memorytoregisterpass_1253',['MemoryToRegisterPass',['../classMemoryToRegisterPass.html',1,'']]],
-  ['mergestorespass_1254',['MergeStoresPass',['../classMergeStoresPass.html',1,'']]],
-  ['mod_1255',['Mod',['../structtags_1_1TaggedType_1_1Mod.html',1,'tags::TaggedType']]],
+  ['maskedtype_1253',['MaskedType',['../classtags_1_1MaskedType.html',1,'tags']]],
+  ['memorytoregisterpass_1254',['MemoryToRegisterPass',['../classMemoryToRegisterPass.html',1,'']]],
+  ['mergestorespass_1255',['MergeStoresPass',['../classMergeStoresPass.html',1,'']]],
   ['moveelimpass_1256',['MoveElimPass',['../classMoveElimPass.html',1,'']]],
   ['movinst_1257',['MovInst',['../classMovInst.html',1,'']]]
 ];
