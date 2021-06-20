0357186dbc22e31b384d371db2c1053cd0cc5aa8
nandor <nl364@cam.ac.uk>
2021-06-20 10:29:29 +0000

    Deploying to gh-pages from  @ ba98346419840a6bff289eb7741e78a2af0efccf 🚀

--- search/classes_15.js
+++ search/classes_15.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['value_1410',['Value',['../classValue.html',1,'Value'],['../structBinaryVisitor_1_1Value.html',1,'BinaryVisitor&lt; T &gt;::Value']]],
+  ['value_1410',['Value',['../structBinaryVisitor_1_1Value.html',1,'BinaryVisitor&lt; T &gt;::Value'],['../classValue.html',1,'Value']]],
   ['value_5fop_5fiterator_1411',['value_op_iterator',['../structUser_1_1value__op__iterator.html',1,'User']]],
   ['valuenumbering_1412',['ValueNumbering',['../classValueNumbering.html',1,'']]],
   ['valuenumberingpass_1413',['ValueNumberingPass',['../classValueNumberingPass.html',1,'']]],
-  ['verifierpass_1414',['VerifierPass',['../classVerifierPass.html',1,'']]]
+  ['verifier_1414',['Verifier',['../classVerifier.html',1,'']]]
 ];
--- search/functions_14.js
+++ search/functions_14.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['value_2042',['Value',['../classValue.html#a00c9868cae7df8e3a4edd9c990679325',1,'Value::Value(Kind kind)'],['../classValue.html#ace0ebc855b0ebd7fa7d90ab54beb386b',1,'Value::Value(const Value &amp;)=delete'],['../classValue.html#aeeb9f4e0c39e659c9315b9b763893c57',1,'Value::Value(Value &amp;&amp;)=delete']]],
   ['valuenumberingpass_2043',['ValueNumberingPass',['../classValueNumberingPass.html#a0dcbfebb9e79a8d74b57fa403f16b046',1,'ValueNumberingPass']]],
-  ['verifierpass_2044',['VerifierPass',['../classVerifierPass.html#a822d39a443bc47dcebf0b15106b18a18',1,'VerifierPass']]],
+  ['verifier_2044',['Verifier',['../classVerifier.html#a7ee851b1cdb75fec309da82027db45a8',1,'Verifier::Verifier(const Target *target)'],['../classVerifier.html#a1bd9f3aea3e1cccd31822984e2fb8212',1,'Verifier::Verifier(Target *target)']]],
   ['verify_2045',['Verify',['../classProgReducerBase.html#a2c94319e4bc215fedf8dfa8854f05014',1,'ProgReducerBase::Verify()'],['../classProgReducer.html#aa57b1fd07a61764f2c1b2b554814e27d',1,'ProgReducer::Verify()']]],
   ['vreg_2046',['VReg',['../classLexer.html#a52f01b5b5aab30d834c33a73aec4f7c0',1,'Lexer']]]
 ];
