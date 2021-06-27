7466f63950ceb9556a1ef34b9f73798dafe2e364
nandor <nl364@cam.ac.uk>
2021-06-27 18:22:58 +0000

    Deploying to gh-pages from  @ 2da8342d1807c42092f13250b9d2276a4a3be422 🚀

--- search/classes_15.js
+++ search/classes_15.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['value_1414',['Value',['../classValue.html',1,'Value'],['../structBinaryVisitor_1_1Value.html',1,'BinaryVisitor&lt; T &gt;::Value']]],
+  ['value_1414',['Value',['../structBinaryVisitor_1_1Value.html',1,'BinaryVisitor&lt; T &gt;::Value'],['../classValue.html',1,'Value']]],
   ['value_5fop_5fiterator_1415',['value_op_iterator',['../structUser_1_1value__op__iterator.html',1,'User']]],
   ['valuenumbering_1416',['ValueNumbering',['../classValueNumbering.html',1,'']]],
   ['valuenumberingpass_1417',['ValueNumberingPass',['../classValueNumberingPass.html',1,'']]],
