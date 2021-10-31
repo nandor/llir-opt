8c589a4778c1a5e170b40f061ed2ee38409e845d
Nandor Licker <n@ndor.email>
2021-10-31 16:49:18 +0200

    [liir-opt] Merged xtors into a single function

--- passes/link.cpp
+++ passes/link.cpp
@@ -35,67 +35,112 @@ static void ZeroExtern(Extern *ext)
 }
 
 // -----------------------------------------------------------------------------
+static bool IsInitFini(llvm::StringRef name)
+{
+  return name == "__init_array_start"
+      || name == "__init_array_end"
+      || name == "__fini_array_start"
+      || name == "__fini_array_end";
+}
+
+// -----------------------------------------------------------------------------
+using XtorMap = std::map<int, std::vector<Func *>>;
+
+// -----------------------------------------------------------------------------
+static Func *MergeXtors(Prog &prog, const char *name, const XtorMap &xtors)
+{
+  Func *xtor = new Func(name);
+  prog.AddFunc(xtor);
+
+  Block *end = new Block(".Lend");
+  end->AddInst(new ReturnInst({}, {}));
+  xtor->AddBlock(end);
+
+  for (auto it = xtors.rbegin(); it != xtors.rend(); ++it) {
+    for (auto ft = it->second.rbegin(); ft != it->second.rend(); ++ft) {
+      Func *f = *ft;
+
+      auto *next = &xtor->getEntryBlock();
+      auto *block = new Block((".Lcall" + f->getName()).str());
+      xtor->AddBlock(block, next);
+
+      auto *mov = new MovInst(Type::I64, f, {});
+      block->AddInst(mov);
+      block->AddInst(new CallInst(
+          {},
+          mov,
+          {},
+          {},
+          next,
+          std::nullopt,
+          CallingConv::C,
+          {}
+      ));
+    }
+  }
+  return xtor;
+}
+
+// -----------------------------------------------------------------------------
 bool LinkPass::Run(Prog &prog)
 {
   bool changed = false;
-
-  Xtor *ctor = nullptr;
-  Xtor *dtor = nullptr;
-  for (auto it = prog.xtor_begin(); it != prog.xtor_end(); ) {
-    Xtor *xtor = &*it++;
-    switch (xtor->GetKind()) {
-      case Xtor::Kind::CTOR: {
-        if (ctor) {
-          llvm::report_fatal_error("duplicate ctor");
-        }
-        ctor = xtor;
-        break;
-      }
-      case Xtor::Kind::DTOR: {
-        if (dtor) {
-          llvm::report_fatal_error("duplicate dtor");
-        }
-        dtor = xtor;
-        break;
-      }
-    }
-  }
-
   auto &cfg = GetConfig();
   if (cfg.Static) {
-    const std::string entry = cfg.Entry.empty() ? "_start" : cfg.Entry;
+    XtorMap ctors, dtors;
+    for (auto it = prog.xtor_begin(); it != prog.xtor_end(); ) {
+      Xtor *xtor = &*it++;
+      switch (xtor->GetKind()) {
+        case Xtor::Kind::CTOR: {
+          ctors[xtor->GetPriority()].push_back(xtor->GetFunc());
+          xtor->eraseFromParent();
+          continue;
+        }
+        case Xtor::Kind::DTOR: {
+          dtors[xtor->GetPriority()].push_back(xtor->GetFunc());
+          xtor->eraseFromParent();
+          continue;
+        }
+      }
+      llvm_unreachable("unknown xtor kind");
+    }
+
+    Func *ctor = MergeXtors(prog, "_init$merge", ctors);
+    Func *dtor = MergeXtors(prog, "_fini$merge", dtors);
+
     for (auto it = prog.ext_begin(); it != prog.ext_end(); ) {
       Extern *ext = &*it++;
+      // Delete externs with no uses.
       if (ext->use_empty() && !::cast_or_null<Constant>(ext->GetValue())) {
         ext->eraseFromParent();
         changed = true;
         continue;
       }
 
-      // If there are no constructors/destructors, zero the arrays.
-      if (!ctor && ext->getName() == "__init_array_start") {
-        ZeroExtern(ext);
+      // Replace _init/_fini with ctor/dtor.
+      if (ext->getName() == "_init") {
+        if (ctor) {
+          ext->replaceAllUsesWith(ctor);
+          ext->eraseFromParent();
+        } else {
+          ZeroExtern(ext);
+        }
         changed = true;
         continue;
       }
-      if (!ctor && ext->getName() == "__init_array_end") {
-        ZeroExtern(ext);
-        changed = true;
-        continue;
-      }
-      if (!dtor && ext->getName() == "__fini_array_start") {
-        ZeroExtern(ext);
-        changed = true;
-        continue;
-      }
-      if (!dtor && ext->getName() == "__fini_array_end") {
-        ZeroExtern(ext);
+      if (ext->getName() == "_fini") {
+        if (ctor) {
+          ext->replaceAllUsesWith(dtor);
+          ext->eraseFromParent();
+        } else {
+          ZeroExtern(ext);
+        }
         changed = true;
         continue;
       }
 
       // Weak symbols can be zeroed here.
-      if (ext->IsWeak()) {
+      if (ext->IsWeak() || IsInitFini(ext->getName())) {
         ZeroExtern(ext);
         changed = true;
         continue;
@@ -111,6 +156,7 @@ bool LinkPass::Run(Prog &prog)
       }
     }
 
+    const std::string entry = cfg.Entry.empty() ? "_start" : cfg.Entry;
     for (Func &func : prog) {
       auto name = func.GetName();
       if (name != entry && name != "caml_garbage_collection") {
